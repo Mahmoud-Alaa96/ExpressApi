fe42ee7a01b4fae1bd0929d621b39c71553fd658
Mahmoud-Alaa96 <jolan133st@gmail.com>
2023-09-19 22:32:08 +0300

    MongoDB connect

---
+++ db.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const doc = {
+    name: "mo"
+  }
+
+
+
+// Database Name
+const dbName = 'express-app';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const users = db.collection('users');
+  const insertResult = await users.insertOne(doc)
+  console.log('Inserted documents =>', insertResult);
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+
+
+
+
+
+
+  
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('./db');
 const userRouter = require('./router/user');
 const todoRouter = require('./router/todo');
 const dnsRouter = require('./router/dns');
